a60c11fe887d5a1634e56e06a10546626dafe792
Yiannis <ioannis.roussos@hotmail.com>
2025-12-08 20:28:57 +0200

    updated

--- src/components/Searchbox.jsx
+++ src/components/Searchbox.jsx
@@ -5,9 +5,23 @@ import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
 const Searchbox = () => {
+    const { id } = useParams();
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [searchId, setSearchId] = useState(''); 
+    
+    async function fetchMovies(searchId) {
+        setLoading(true)
+        const {data} =
+        await axios.get(`https://www.omdbapi.com/?s=${searchId}&apikey=279e89f2`);
+        setMovies(data || []);
+        setLoading(false);
+        console.log (data);
+    }
+    
     function handleSubmit(event) {
         event.preventDefault(); // stop the page refresh
-        
+        fetchMovies({searchId});
         // run your animation delay here if you want
         
         setTimeout(() => {
@@ -34,20 +48,8 @@ const Searchbox = () => {
     
 )
 
-    const { id } = useParams();
-    const [movies, setMovies] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [searchId, setSearchId] = useState(id || ''); 
   
 
-    async function fetchMovies(searchId) {
-        setLoading(true)
-        const {data} =
-        await axios.get(`http://www.omdbapi.com/?s=${searchId}&apikey=279e89f2`);
-        setMovies(data.Search || []);
-        setLoading(false);
-        console.log(data);
-    }
 
 }
 
--- src/components/scrap.jsx
+++ src/components/scrap.jsx
@@ -1,93 +1,93 @@
-import React, { useState, useEffect } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import axios from 'axios'
+// import React, { useState, useEffect } from 'react'
+// import { Link, useParams } from 'react-router-dom'
+// import axios from 'axios'
 
-const Scrap = () => {
+// const Scrap = () => {
     
-    const { id } = useParams();
-    const [movies, setMovies] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [searchId, setSearchId] = useState(id || '');
+//     const { id } = useParams();
+//     const [movies, setMovies] = useState([]);
+//     const [loading, setLoading] = useState(false);
+//     const [searchId, setSearchId] = useState(id || '');
     
-    async function fetchMovies(searchId) {
-        setLoading(true)
-        const {data} =
-        await axios.get(`http://www.omdbapi.com/?s=${searchId}&apikey=279e89f2`);
-        setMovies(data.Search || []);
-        setLoading(false);
-        console.log(data);
-    }
+//     async function fetchMovies(searchId) {
+//         setLoading(true)
+//         const {data} =
+//         await axios.get(`http://www.omdbapi.com/?s=${searchId}&apikey=279e89f2`);
+//         setMovies(data.Search || []);
+//         setLoading(false);
+//         console.log(data);
+//     }
     
-    function onSearch() {
-        fetchMovies(searchId)
-        console.log()
-    }
-    function onSearchKeyPress(key) {
-        if (key === 'Enter') {
-            onSearch();
-        }
-    }
-    useEffect(() => {
+//     function onSearch() {
+//         fetchMovies(searchId)
+//         console.log()
+//     }
+//     function onSearchKeyPress(key) {
+//         if (key === 'Enter') {
+//             onSearch();
+//         }
+//     }
+//     useEffect(() => {
         
-        fetchMovies();
-    }, []);
+//         fetchMovies();
+//     }, []);
     
-  return (
-    <>
-    <div className="search__input">
-      <input
-        type='search' value={searchId} onChange={(event) => setSearchId(event.target.value)} onKeyPress={(event) => onSearchKeyPress(event.key)}
-      />
-      <button className='search__bar button' onClick={() => onSearch()}>Enter</button> 
-     </div>
+//   return (
+//     <>
+//     <div className="search__input">
+//       <input
+//         type='search' value={searchId} onChange={(event) => setSearchId(event.target.value)} onKeyPress={(event) => onSearchKeyPress(event.key)}
+//       />
+//       <button className='search__bar button' onClick={() => onSearch()}>Enter</button> 
+//      </div>
 
-     {
-    loading 
-    ? new Array(10).fill(0).map((_, data) => (
-        <div className="movie" key={data.id}>
-    <div className="movie__title">
-      <div className="movie__title--skeleton"></div>
-    </div>
-    <div className="movie__body">
-      <p className="movie__body--skeleton"></p>
-    </div>
-  </div>
-    )): 
+//      {
+//     loading 
+//     ? new Array(10).fill(0).map((_, data) => (
+//         <div className="movie" key={data.id}>
+//     <div className="movie__title">
+//       <div className="movie__title--skeleton"></div>
+//     </div>
+//     <div className="movie__body">
+//       <p className="movie__body--skeleton"></p>
+//     </div>
+//   </div>
+//     )): 
 
-        movies.map((movie)=>(
-      <div className="movie" key={movie.id}>
-    <div className="post__title">{movie.title}</div>
-    <p className="post__body">{movie.body}</p>
-  </div>
-      ))
+//         movies.map((movie)=>(
+//       <div className="movie" key={movie.id}>
+//     <div className="post__title">{movie.title}</div>
+//     <p className="post__body">{movie.body}</p>
+//   </div>
+//       ))
     
-  }
-    {/* <div className='movie__search'>
-      <Link to={'/'}>
-      <button>Back</button>
-        </Link>
-      <div className="container">
-    <div className="row">
-        <div className="movie-list">
-            <div className="movie">
-                <div className="movie-card__container">
-                    <div className="movie-card">
-                    <div class="movie-card">
-                        <h3><b>Title:</b> Title </h3>
-                        <p><b>Type:</b> Movie</p>
-                        <p className="imdb__link"><b>ImdbID</b></p>
+//   }
+//     {/* <div className='movie__search'>
+//       <Link to={'/'}>
+//       <button>Back</button>
+//         </Link>
+//       <div className="container">
+//     <div className="row">
+//         <div className="movie-list">
+//             <div className="movie">
+//                 <div className="movie-card__container">
+//                     <div className="movie-card">
+//                     <div class="movie-card">
+//                         <h3><b>Title:</b> Title </h3>
+//                         <p><b>Type:</b> Movie</p>
+//                         <p className="imdb__link"><b>ImdbID</b></p>
                       
-                      <img src="" alt="" />
-                    </div>
-                 </div>
-          </div>
-        </div>
-      </div>
-    </div> */}
-    {/* </div>
-    </div> */}
-     </>
-  )
-}
+//                       <img src="" alt="" />
+//                     </div>
+//                  </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div> */}
+//     {/* </div>
+//     </div> */}
+//      </>
+//   )
+// }
 
-export default Scrap
+// export default Scrap
