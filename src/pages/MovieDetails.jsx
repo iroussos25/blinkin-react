c6f4d28dfea9b173e6f7e4d0a6f7d19f784fb6d3
Yiannis <ioannis.roussos@hotmail.com>
2025-12-11 10:31:26 +0200

    updated

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -5,57 +5,69 @@ import blinkin from '../assets/blinkin-Photoroom.png'
 import './MovieDetails.css'
 
 const MovieDetails = () => {
-  return (
-    <div className="movies__body">   
-        <main className="movies__main">
-            <div className="movies__container">
-                <div className="row">
-                    <div className="movie__selected--top">
-                        <Link to="/" className="home__link">
-                            <FontAwesomeIcon icon="arrow-left" />
-                        </Link>   
-                        {/* <Link to="/books" className="book__link">
-                        <h2 className="book__selected--title--top">
-                            Books</h2>
-                        </Link>   */}
-                    </div>
-                    <div className="movie__selected">
-                        <figure className="movie__selected--figure">
-                            <img src={blinkin} alt="" className="movie__selected--img"/>
-                        </figure>
-                        <div className="movie__selected--description">
-                            <h2 className="movie__selected--title">Title</h2>
-                            <h2 className="movie__selected--rating">Rating</h2>
-                            
-                            <div className="movie__summary">
-                                <h3 className="movie__summary--title">
-                                    Summary
-                                </h3>
-                                <p className="movie__summary--para">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt corrupti nulla voluptatum corporis eum qui minus, odio repudiandae rerum quae.
-                                </p>
-                                <p className="movie__summary--director">
-                                    Director
-                                </p>
-                                <p className="movie__summary--starring">
-                                  Starring                                
-                                  </p>
-                                <p className="movie__summary--rating">
-                                    Rating
-                                </p>
-                                <p className="movie__summary--genre">
-                                    Genre
-                                </p>
+    return (
+    async function getPlot() {
+        const movies = await fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&plot=full&apikey=279e89f2`)
+        const plot = await movies.json
+        console.log(plot)
+})
+    }
 
-                            </div>
+    // function displayPlot(movie){
+        // return
+    // }
+
+//     <div className="movies__body">   
+//         <main className="movies__main">
+//             <div className="movies__container">
+//                 <div className="row">
+//                     <div className="movie__selected--top">
+//                         <Link to="/" className="home__link">
+//                             <FontAwesomeIcon icon="arrow-left" />
+//                         </Link>   
+//                         {/* <Link to="/books" className="book__link">
+//                         <h2 className="book__selected--title--top">
+//                             Books</h2>
+//                         </Link>   */}
+//                     </div>
+//                     <div className="movie__selected">
+//                         <figure className="movie__selected--figure">
+//                             <img src={blinkin} alt="" className="movie__selected--img"/>
+//                         </figure>
+//                         <div className="movie__selected--description">
+//                             <h2 className="movie__selected--title">Title</h2>
+//                             <h2 className="movie__selected--rating">Rating</h2>
                             
-                        </div>
-                    </div>
-                </div>
-            </div>
-</main>
-</div>
-  )
-}
+//                             <div className="movie__summary">
+//                                 <h3 className="movie__summary--title">
+//                                     Summary
+//                                 </h3>
+//                                 <p className="movie__summary--para">
+//                                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt corrupti nulla voluptatum corporis eum qui minus, odio repudiandae rerum quae.
+//                                 </p>
+//                                 <p className="movie__summary--director">
+//                                     Director
+//                                 </p>
+//                                 <p className="movie__summary--starring">
+//                                   Starring                                
+//                                   </p>
+//                                 <p className="movie__summary--rating">
+//                                     Rating
+//                                 </p>
+//                                 <p className="movie__summary--genre">
+//                                     Genre
+//                                 </p>
+//                                 <a href={`http://www.omdbapi.com/?i=${movie.imdbID}&plot=full&apikey=279e89f2`} target="_blank">View on IMDb</a>
+
+//                             </div>
+                            
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+// </main>
+// </div>
+//   )
+
 
 export default MovieDetails
