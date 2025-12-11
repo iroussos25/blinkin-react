6bdd1c2a281a414225d63a6c8d52fee3b0bb78d6
Yiannis <ioannis.roussos@hotmail.com>
2025-12-11 13:25:12 +0200

    searchfunction.jsx issue

--- src/components/SearchFunction.jsx
+++ src/components/SearchFunction.jsx
@@ -1,3 +1,4 @@
+import MovieCard from "./MovieCard";
 
 async function main() {
 //   const buttonEl = document.querySelector('#submit__button');
@@ -60,34 +61,25 @@ async function main() {
 
 
 function movieHTML(movie) {
-//    return `
-    //    <MovieCard/>
-  return `
-    <div className="movie-card__container">
-      <div className="movie-card">
-        <h3>${movie.Title}</h3>
-        <img src="${movie.Poster !== 'N/A' ? movie.Poster : './assets/no-image.jpg'}" alt="${movie.Title}" />
-        <p><b>Type:</b> ${movie.Type}</p>
-        <p><b>Year:</b> ${movie.Year}</p>
-        <p className="imdb__link">
-          <b>imdbID:</b> ${movie.imdbID} 
-          <a href="https://www.imdb.com/title/${movie.imdbID}/" target="_blank">View on IMDb</a>
-        </p>
-      </div>
-    </div>
-  `;
+   return 
+       <MovieCard/>
+  
+//     return `
+//     <div className="movie-card__container">
+//       <div className="movie-card">
+//         <h3>${movie.Title}</h3>
+//         <img src="${movie.Poster !== 'N/A' ? movie.Poster : './assets/no-image.jpg'}" alt="${movie.Title}" />
+//         <p><b>Type:</b> ${movie.Type}</p>
+//         <p><b>Year:</b> ${movie.Year}</p>
+//         <p className="imdb__link">
+//           <b>imdbID:</b> ${movie.imdbID} 
+//           <a href="https://www.imdb.com/title/${movie.imdbID}/" target="_blank">View on IMDb</a>
+//         </p>
+//       </div>
+//     </div>
+//   `;
 }
 
-// Initialize when page loads
-main();
 
-function openMenu() {
-document.body.classList += " menu--open"
-}
-
-function closeMenu() {
-document.body.classList.remove ('menu--open')
-
-}
 
 // export default (main, openMenu, closeMenu, movieHTML)
